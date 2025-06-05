540df36453b437fdbe268f06a660b845c8dfc0ac
Adarsh Pandey <learneradarsh@users.noreply.github.com>
2025-06-05 11:51:32 +0530

    Add Three.js and GSAP setup

--- build/scripts.js
+++ build/scripts.js
@@ -3,21 +3,34 @@ const uglify = require('gulp-uglify');
 
 const jsPath = '_scripts/*.js';
 const destPath = '_site/js';
+const libs = [
+  'node_modules/three/build/three.module.js',
+  'node_modules/gsap/dist/gsap.min.js',
+];
+const libsDest = `${destPath}/lib`;
 
 module.exports = gulp => {
-  gulp.task('scripts', () => {
-    return (
-      gulp
-        .src(jsPath)
-        .pipe(
-          eslint({
-            useEslintrc: true,
-          })
-        )
-        .pipe(eslint.format())
-        // .pipe(uglify())
-        .pipe(gulp.dest(destPath))
-        .pipe(gulp.dest('js'))
-    );
+  // Copy Three.js and GSAP into js/lib and _site/js/lib
+  gulp.task('copy-libs', () => {
+    return gulp
+      .src(libs)
+      .pipe(gulp.dest('js/lib'))
+      .pipe(gulp.dest(libsDest));
   });
+
+  const compileScripts = () => {
+    return gulp
+      .src(jsPath)
+      .pipe(
+        eslint({
+          useEslintrc: true,
+        })
+      )
+      .pipe(eslint.format())
+      // .pipe(uglify())
+      .pipe(gulp.dest(destPath))
+      .pipe(gulp.dest('js'));
+  };
+
+  gulp.task('scripts', gulp.series('copy-libs', compileScripts));
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('./build/sass');
+// Handles custom JS and copies external libraries
 const scripts = require('./build/scripts');
 const images = require('./build/images');
 const sync = require('./build/browsersync');
