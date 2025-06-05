9221af1bf3ac98b1c697848fad1a0e3f2eb8d301
Adarsh Pandey <learneradarsh@users.noreply.github.com>
2025-06-05 11:57:45 +0530

    Remove dark/light theme toggle

--- _scripts/main.js
+++ _scripts/main.js
@@ -3,22 +3,11 @@ $(function() {
   const hours = d.getHours();
   const night = hours >= 19 || hours <= 7; // between 7pm and 7am
   const body = document.querySelector('body');
-  const toggle = document.getElementById('toggle');
-  const input = document.getElementById('switch');
+  /* theme toggle removed */
 
   if (night) {
-    input.checked = true;
     body.classList.add('night');
   }
-
-  toggle.addEventListener('click', function() {
-    const isChecked = input.checked;
-    if (isChecked) {
-      body.classList.remove('night');
-    } else {
-      body.classList.add('night');
-    }
-  });
 
   const introHeight = document.querySelector('.intro').offsetHeight;
   const topButton = document.getElementById('top-button');
