da10f38e86d4d520bb81958351ba7224fa06de36
Adarsh Pandey <learneradarsh@users.noreply.github.com>
2025-06-06 12:22:32 +0530

    Remove three.js intro animation and email modal

--- js/main.js
+++ js/main.js
@@ -110,23 +110,4 @@ $(function() {
     });
   }
 
-  const resumeLink = document.getElementById('resume-link');
-  const resumeModal = document.getElementById('resume-modal');
-  const modalClose = document.getElementById('modal-close');
-  const resumeForm = document.getElementById('resume-form');
-
-  if (resumeLink && resumeModal && modalClose && resumeForm) {
-    resumeLink.addEventListener('click', e => {
-      e.preventDefault();
-      resumeModal.classList.add('show');
-    });
-    modalClose.addEventListener('click', () => {
-      resumeModal.classList.remove('show');
-    });
-    resumeForm.addEventListener('submit', e => {
-      e.preventDefault();
-      resumeModal.classList.remove('show');
-      window.open('https://drive.google.com/file/d/1AX5VMlGrS8XHuldPwF3mcw_GWYdynL2g/view?usp=sharing', '_blank');
-    });
-  }
 });
