b879a3ceeb8319d80746207799d6f268273f2ec2
learneradarsh <learner.adarsh@gmail.com>
2026-01-04 16:44:27 +0530

    Refactor: Switch to Tailwind CDN for simpler styling setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
-
 export default defineConfig({
   base: "/",
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
 });
